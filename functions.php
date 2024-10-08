c5b374297728a65d303e4b8443f2474d9bc9a8f8
Austin <sales@automachinespecialties.com>
2024-10-08 09:33:59 -0700

    feat(clock): put post back DOH cleaned up too

--- functions.php
+++ functions.php
@@ -16,6 +16,36 @@ add_action("rest_api_init", function(){
 	
 	register_rest_route("track-time/v1", "/invoice", [
 		[
+			"methods"	=> "POST",
+			"callback"	=> function(WP_REST_Request $req){
+				global $wpdb;
+				$invoice			= $req->get_param("invoice");
+				$whois				= $req->get_param("whois");
+				$manTime			= $req->get_param("manTime");
+				$start				= $req->get_param("start");
+				if(!is_numeric($start)){
+					$start 			= strtotime($start);
+				}
+				$trackArr 			= [];
+				$trackArr["time"]	= $manTime ? $manTime : $req->get_param("time");
+				$trackArr["notes"]	= $req->get_param("notes");
+				$trackObj			= json_encode($trackArr);
+
+				$query				= $wpdb->prepare("INSERT INTO wp_track_time (invoice, time, employee, date) VALUES ('{$invoice}', '{$trackObj}', {$whois}, FROM_UNIXTIME({$start}))");
+				$success 			= $wpdb->query($query);
+
+				return [
+					"success"		=> $success,
+				];
+			},
+			"permission_callback"	=> function(){
+				$nonce = sanitize_text_field($_SERVER["HTTP_X_WP_NONCE"]);
+				if(wp_verify_nonce($nonce, "wp_rest")){
+					return true;
+				}
+			}
+		],
+		[
 			"methods"	=> "DELETE",
 			"callback"	=> function(WP_REST_Request $req){
 				global $wpdb;
