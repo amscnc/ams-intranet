143825a02973cd0e5f1f746a13faeebf26a904a2
Austin <sales@automachinespecialties.com>
2024-10-09 11:26:07 -0700

    feat(clock): new database working, menus working. Could be structured better

--- admin-employee.php
+++ admin-employee.php
@@ -52,77 +52,10 @@ add_submenu_page(
 							method: "GET",
 						})
 						.then(res=>res.json())
-						.then(obj=>popRecord(obj.response))
+						.then(obj=>popRecord(obj.response, date.value))
 					})
-
-					function popRecord(data){
-						console.log(data)
-						while(timeList.firstChild){
-							timeList.removeChild(timeList.lastChild)
-						}
-
-						let employees 		= [...new Set(data.map(entries => entries[3]))];
-						const employeeList 	= document.createElement("ul")
-						for(const employee of employees){
-							console.log("employee:", employee)
-							const employeeItem			= document.createElement("li")
-							const employeeHeader		= document.createElement("h3")
-							employeeHeader.innerText	= `Employee: ${employee}`
-							employeeItem.appendChild(employeeHeader)
-							const entryList 			= document.createElement("ul")
-							for(const entry of data){
-								if(entry[3] == employee){
-									const entryData = JSON.parse(entry[2])
-									const entryItem = document.createElement("li")
-									const date = new Date(entry[4])
-									const dateElement = document.createElement("p")
-									dateElement.innerText = date.toLocaleString()
-									entryItem.appendChild(dateElement)
-									const time = document.createElement("p")
-									time.innerText = `Time: ${entryData.time}`
-									entryItem.appendChild(time)
-									if(entryData.notes){
-										const notes = document.createElement("p")
-										notes.innerText = entryData.notes
-										entryItem.appendChild(notes)
-									}
-									
-									const deleteBtn = document.createElement("button")
-									deleteBtn.innerText = "Delete"
-									deleteBtn.addEventListener("click", ()=>deleteRecord(i, x))
-									entryItem.appendChild(deleteBtn)
-									entryList.appendChild(entryItem)
-								}
-							}
-							employeeItem.appendChild(entryList)
-							employeeList.appendChild(employeeItem)
-						}
-						timeList.appendChild(employeeList)
-					}
-
-					function deleteRecord(employee, start){
-						const bool = confirm("Are you sure you want to delete?")
-						if(bool){
-							fetch("<?=get_rest_url()?>track-time/v1/invoice",{
-								method: "UPDATE",
-								headers: {
-									"Content-Type": "application/json",
-									"X-WP-Nonce": "<?=wp_create_nonce("wp_rest");?>",
-								},
-								body: JSON.stringify({
-									invoice: search.value,
-									start,
-									employee,
-								})
-							})
-							.then(res=>res.json())
-							.then(obj=>{
-								console.log(obj)
-								popRecord(obj)
-							})
-						}
-					}
 				</script>
+				<?php require get_template_directory()."/popRecord.php";?>
 			<?php
 		}
 	);
---
+++ clock-api.php
@@ -0,0 +1,137 @@
+<?php
+
+function get_by_invoice($invoice){
+    global $wpdb;
+    $result     = $wpdb->get_results("SELECT * FROM wp_track_time WHERE invoice = '{$invoice}'", ARRAY_N);
+    return $result;
+}
+
+function get_by_employee($employee, $date){
+    global $wpdb;
+    $result 	= $wpdb->get_results("SELECT * FROM wp_track_time WHERE employee = {$employee} and DATE(date) = '{$date}'", ARRAY_N);
+    return $result;
+}
+
+register_rest_route("track-time/v1", "/invoice", [
+    "methods"	=> "GET",
+    "callback"	=> function(WP_REST_Request $req){
+        $result = get_by_invoice($req->get_param("invoice"));
+        return [
+            "response"	=> $result,
+        ];
+    }
+]);
+
+register_rest_route("track-time/v1", "/employee", [
+    "methods"	=> "GET",
+    "callback"	=> function(WP_REST_Request $req){
+        $result = get_by_employee($req->get_param("whois"), $req->get_param("date"));
+        return [
+            "response"	=> $result,
+        ];
+    }
+]);
+
+register_rest_route("track-time/v1", "/invoice", [
+    [
+        "methods"	=> "POST",
+        "callback"	=> function(WP_REST_Request $req){
+            global $wpdb;
+            $invoice			= $req->get_param("invoice");
+            $whois				= $req->get_param("whois");
+            $manTime			= $req->get_param("manTime");
+            $start				= $req->get_param("start");
+            if(!is_numeric($start)){
+                $start 			= strtotime($start);
+            }
+            $trackArr 			= [];
+            $trackArr["time"]	= $manTime ? $manTime : $req->get_param("time");
+            $trackArr["notes"]	= $req->get_param("notes");
+            $trackObj			= json_encode($trackArr);
+
+            $query				= $wpdb->prepare("INSERT INTO wp_track_time (invoice, time, employee, date) VALUES ('{$invoice}', '{$trackObj}', {$whois}, FROM_UNIXTIME({$start}))");
+            $success 			= $wpdb->query($query);
+
+            return [
+                "success"		=> $success,
+            ];
+        },
+        "permission_callback"	=> function(){
+            $nonce = sanitize_text_field($_SERVER["HTTP_X_WP_NONCE"]);
+            if(wp_verify_nonce($nonce, "wp_rest")){
+                return true;
+            }
+        }
+    ],
+    [
+        "methods"	=> "DELETE",
+        "callback"	=> function(WP_REST_Request $req){
+            global $wpdb;
+
+            $id			= $req->get_param("id");
+            $query		= $wpdb->prepare("DELETE FROM wp_track_time WHERE id = {$id}");
+            $success	= $wpdb->query($query);
+
+            $date           = $req->get_param("date");
+            $result;
+            if($date){
+                $employee   = $req->get_param("employee");
+                $result     = get_by_employee($employee, $date);
+            }else{
+                $invoice    = $req->get_param("invoice");
+                $result     = get_by_invoice($invoice);
+            }
+
+            return [
+                "success"	=> $success,
+                "response"	=> $result,
+            ];
+        },
+        "permission_callback"	=> function(){
+            $nonce = sanitize_text_field($_SERVER["HTTP_X_WP_NONCE"]);
+            if(wp_verify_nonce($nonce, "wp_rest")){
+                return true;
+            }
+        }
+    ],
+    [
+        "methods"	=> "UPDATE",
+        "callback"	=> function(WP_REST_Request $req){
+            global $wpdb;
+            $invoice = $req->get_param("invoice");
+            $current = $wpdb->get_results("SELECT time FROM wp_track_time WHERE invoice = '{$invoice}'", ARRAY_N);
+            $array = json_decode($current[0][0], true);
+
+            // foreach($array as $key => $value){
+            // 	if($key == $req->get_param("employee")){
+            // 		$result[] = $key;
+            // 		if(count($value) > 1){
+            // 			foreach($value as $start => $time){
+            // 				if($start == $req->get_param("start")){
+            // 					$result[] = $start;
+            // 					unset($array[$key][$start]);
+            // 				}
+            // 			}
+            // 		}else{
+            // 			unset($array[$key]);
+            // 		}
+            // 	}
+            // }
+            // $update = json_encode($array);
+            // $query = $wpdb->prepare("UPDATE wp_track_time SET time = '{$update}' WHERE invoice = '{$invoice}'");
+            // $success = $wpdb->query($query);
+
+            return [
+                // "success"	=> $success,
+                // "invoice"	=> $array,
+                "response"		=> "nothing right now"
+            ];
+        },
+        "permission_callback"	=> function(){
+            $nonce = sanitize_text_field($_SERVER["HTTP_X_WP_NONCE"]);
+            if(wp_verify_nonce($nonce, "wp_rest")){
+                return true;
+            }
+        }
+    ],
+]);
